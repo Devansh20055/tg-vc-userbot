31d2b00e82c4b9338e2c1d29ac61cb9b2e950ca1
Mᴏᴏɴʟɪɢʜᴛ <87492539+Lost-In-Dark@users.noreply.github.com>
2021-11-10 06:29:49 +0530

    Update play.py

--- VCBot/play.py
+++ VCBot/play.py
@@ -48,7 +48,7 @@ async def ytdl(link):
 
 @Client.on_message(filters.command(['play'], prefixes=f"{HNDLR}"))
 async def play(client, m: Message):
- if not GRPPLAY and not (m.outgoing or (m.from_user and m.from_user.is_contact)):
+ if not GRPPLAY and (not m.outgoing or not m.from_user.is_contact):
    pass
  else:
    replied = m.reply_to_message
