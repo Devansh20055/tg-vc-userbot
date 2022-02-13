d21fc13df1b6e8945dc6650673a23f83b0ccfb94
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-02-13 06:58:04 +0530

    Update skip.py

--- VCBot/skip.py
+++ VCBot/skip.py
@@ -11,13 +11,13 @@ async def skip(client, m: Message):
    if len(m.command) < 2:
       op = await skip_current_song(chat_id)
       if op==0:
-         await m.reply("`Nothing Is Playing`")
+         await m.reply("`Nothing Is Playing`\n @TEAM_SILENT_KING")
       elif op==1:
-         await m.reply("`Queue is Empty, Leaving Voice Chat...`")
+         await m.reply("`Queue is Empty, Leaving Voice Chat...`\n @TEAM_SILENT_KING")
       elif op==2:
-         await m.reply(f"**Some Error Occurred** \n`Clearing the Queues and Leaving the Voice Chat...`")
+         await m.reply(f"**Some Error Occurred** \n`Clearing the Queues and Leaving the Voice Chat...` HEU HUE ")
       else:
-         await m.reply(f"**Skipped ⏭** \n**🎧 Now Playing** - [{op[0]}]({op[1]}) | `{op[2]}`", disable_web_page_preview=True)
+         await m.reply(f"**Skipped ⏭** \n**🎧 Now Playing** - [{op[0]}]({op[1]}) | `{op[2]}`\n @TEAM_SILENT_KING", disable_web_page_preview=True)
    else:
       skip = m.text.split(None, 1)[1]
       OP = "**Removed the following songs from Queue:-**"
@@ -42,11 +42,11 @@ async def stop(client, m: Message):
       try:
          await call_py.leave_group_call(chat_id)
          clear_queue(chat_id)
-         await m.reply("**Stopped Streaming ⏹️**")
+         await m.reply("**Stopped Streaming ⏹️**\n @TEAM_SILENT_KING")
       except Exception as e:
          await m.reply(f"**ERROR** \n`{e}`")
    else:
-      await m.reply("`Nothing is Streaming`")
+      await m.reply("`Nothing is Streaming`\n @TEAM_SILENT_KING")
    
 @Client.on_message(contact_filter & filters.command(['pause'], prefixes=f"{HNDLR}"))
 async def pause(client, m: Message):
@@ -54,11 +54,11 @@ async def pause(client, m: Message):
    if chat_id in QUEUE:
       try:
          await call_py.pause_stream(chat_id)
-         await m.reply("**Paused Streaming ⏸️**")
+         await m.reply("**Paused Streaming ⏸️**\n @TEAM_SILENT_KING")
       except Exception as e:
          await m.reply(f"**ERROR** \n`{e}`")
    else:
-      await m.reply("`Nothing is Streaming`")
+      await m.reply("`Nothing is Streaming`\n @TEAM_SILENT_KING")
       
 @Client.on_message(contact_filter & filters.command(['resume'], prefixes=f"{HNDLR}"))
 async def resume(client, m: Message):
@@ -66,8 +66,8 @@ async def resume(client, m: Message):
    if chat_id in QUEUE:
       try:
          await call_py.resume_stream(chat_id)
-         await m.reply("**Resumed Streaming ▶**")
+         await m.reply("**Resumed Streaming ▶**\n @TEAM_SILENT_KING")
       except Exception as e:
          await m.reply(f"**ERROR** \n`{e}`")
    else:
-      await m.reply("`Nothing is Streaming`")
+      await m.reply("`Nothing is Streaming`\n @TEAM_SILENT_KING")
